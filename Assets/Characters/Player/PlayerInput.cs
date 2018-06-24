d9e0c8f7af2acbe20efabb06e5cb4d46d583e4c2
Unknown <deathtouch9@hotmail.com>
2018-06-23 18:47:56 -0700

    Test for Push

---
+++ Assets/Characters/Player/PlayerInput.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine;
+
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class PlayerInput : MonoBehaviour {
+    
+    Player player;
+    ThirdPersonCharacter thirdPersonCharacter = null;
+    float damageCaused = 10;
+    
+
+    //KeyBinds
+    public KeyCode Crouch = KeyCode.Z;
+    public KeyCode SelfDamage = KeyCode.H;
+    public KeyCode Exp = KeyCode.K;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<Player>();
+        thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
+    }
+    public void SetDamage(float damage)
+    {
+        damageCaused = damage;
+    }
+
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        ////crouching
+        //if (Input.GetKey(Crouch))
+        //{
+        //    thirdPersonCharacter.m_Crouching = true;
+        //}
+        //else
+        //{
+        //    thirdPersonCharacter.m_Crouching = false;
+        //}
+
+        //if (thirdPersonCharacter.m_Crouching)
+        //{
+        //    print("Crouching!");
+        //}
+
+        if (Input.GetKeyDown(SelfDamage))
+        {
+            Component damageableComponent = player;
+
+            (damageableComponent as IDamageable).TakeDamage(damageCaused);
+            player.timeSinceLastDamaged = Time.time;
+            if (player.currentHealthPoints <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+        //TODO uncomment after fix EXP
+        //if (Input.GetKeyDown(Exp))
+        //{
+        //   player.experiencePoints += 12;
+        //}
+    }
+}
